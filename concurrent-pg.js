1bb7c0e3df6783f879746e0881bb57d4642311c1
Aidan Reidel <aidan.reidel@gmail.com>
2020-03-08 21:15:08 -0700

    Updated both concurrent-*.js

--- concurrent-pg.js
+++ concurrent-pg.js
@@ -1,16 +1,22 @@
 const { Pool } = require('pg')
 
 // number of concurrent writers
-const NUM_WRITERS = 50
+const NUM_WRITERS = 1
 
 const pool = new Pool({
   max: NUM_WRITERS
 })
 
+let count = 0
+
+// This still doesn't quite work
+// If we can't figure this out use js time instead!
+
 for (let i = 0; i < NUM_WRITERS; i++) {
-  const query = `UPDATE table1 SET string4 = $1 WHERE onepercent = $2`
+  const query = `UPDATE ONEMTUP SET string4 = $1 WHERE fiftypercent = $2`
 
-  pool.query(query, ['test', i])
+  pool.query(query, ['test', 1], () => {
+    count++
+    if (count === NUM_WRITERS) pool.end()
+  })
 }
-
-pool.end()
--- concurrent-sqlite.js
+++ concurrent-sqlite.js
@@ -1,18 +1,16 @@
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require('sqlite3').verbose()
 
-const db_filename = './test.db'
-const db = new sqlite3.Database(db_filename);
+const db_filename = './project.db'
+const db = new sqlite3.Database(db_filename)
 
 // number of concurrent writers
 const NUM_WRITERS = 1
 
 db.parallelize(function() {
   for (let i = 0; i < NUM_WRITERS; i++) {
-      const query = `UPDATE table1 SET string4 = ? WHERE onepercent = ?`    
-      db.run(query, 'hello', i)
+    const query = `UPDATE ONEMTUP SET string4 = ? WHERE fiftypercent = 1`
+    db.run(query, 'test', i)
   }
-});
-
-
+})
 
 db.close()
